b0e30c55ec356902de82eac9f828411930a0c222
MPdev <developperanselin.mp@gmail.com>
2022-02-06 18:29:11 +0100

    feat add crud job routes

---
+++ db/migrate/20220206163923_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.integer :salary
+      t.text :spoken_languages
+      t.text :shift_dates
+      t.timestamps
+    end
+  end
+end
